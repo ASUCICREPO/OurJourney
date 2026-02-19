98186b630440b49c0f9781e0e6b4974f176a2c47
MatthewBrummund <143558807+MatthewBrummund@users.noreply.github.com>
2026-02-18 18:28:32 -0700

    diagram update

--- our-journey-frontend/frontend/src/app/components/constants.jsx
+++ our-journey-frontend/frontend/src/app/components/constants.jsx
@@ -2,5 +2,5 @@
 // This file is a backup template used by the build script
 // DO NOT edit the tokens - they will be replaced during build
 
-export const WEBSOCKET_API = "wss://w03vsws35j.execute-api.us-east-1.amazonaws.com/prod/";
-export const REST_API_URL = "https://titfru0o8l.execute-api.us-east-1.amazonaws.com/prod";
+export const WEBSOCKET_API = "wss://hvo2z2qr9g.execute-api.us-east-1.amazonaws.com/prod/";
+export const REST_API_URL = "https://s8gp0zj83i.execute-api.us-east-1.amazonaws.com/prod/";
